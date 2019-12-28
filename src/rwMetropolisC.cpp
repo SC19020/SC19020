1e9b3fe527facc547672172159c9c8500fcbd76d
yaosuming <smyao@mail.ustc.edu.cn>
2019-12-28 12:08:23 +0800

    ﬁrst commit

---
+++ src/rwMetropolisC.cpp
@@ -0,0 +1,31 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+
+//' @title Use three inputs to generate random walk metropolis samples using Rcpp.
+//' @description Use normal and uniform distribution to do this job
+//' @param sigma the first predictor (numeirc)
+//' @param a the second predictor (numeric)
+//' @param N the third predictor (numeric)
+//' @return a random sample of size \code{N}
+//' @examples
+//' \dontrun{
+//' rw <- rwMetropolisC(2,25,1000)
+//' }
+//' @export
+// [[Rcpp::export]]
+NumericVector rwMetropolisC(double sigma, double a, int N) {
+  NumericVector x(N);
+  x[0] = a;
+  int k = 0;
+  for(int i = 1; i < N; i++){
+    double u = as<double>(runif(1));
+    double y = as<double>(rnorm(1, x[i-1], sigma));
+    if(u <= exp(-abs(y)+abs(x[i-1]))) 
+      x[i] = y;
+    else {
+      x[i] = x[i-1];
+      k += 1;
+    }
+  }
+  return x;
+}
